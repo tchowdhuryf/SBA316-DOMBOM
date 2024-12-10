4446f706fbebcc5f01c93619d50ae8b592eeb8be
tchowdhuryf <tasnim.chowdhury837@gmail.com>
2024-12-10 00:15:04 -0500

    added shuffle function to shuffle cards in the beginning of the game

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const cards = document.querySelectorAll(".card");
 
 let cardFlipped = false;
+let lockBoard = false;
 let card1, card2;
 
 function flipCard() {
+  if (lockBoard) return;
+
   this.classList.add("flip");
 
   if (!cardFlipped) {
@@ -22,11 +25,20 @@ function checkForMatch() {
     card1.removeEventListener("click", flipCard);
     card2.removeEventListener("click", flipCard);
   } else {
+    lockBoard = true;
     setTimeout(() => {
       card1.classList.remove("flip");
       card2.classList.remove("flip");
+      lockBoard = false;
     }, 1000);
   }
 }
+
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos; //order-flex items property
+  });
+})();
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
