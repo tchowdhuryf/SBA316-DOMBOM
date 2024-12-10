356d53916e59c2cf5277718f8fd0083b03c501c0
tchowdhuryf <tasnim.chowdhury837@gmail.com>
2024-12-09 23:28:16 -0500

    added conditions to check if card is flipped and added data to card container

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const cards = document.querySelectorAll(".card");
 
+let cardFlipped = false;
+let card1, card2;
+
 function flipCard() {
-  this.classList.toggle("flip");
+  this.classList.add("flip");
+
+  if (!cardFlipped) {
+    cardFlipped = true;
+    card1 = this;
+  } else {
+    cardFlipped = false;
+    card2 = this;
+  }
 }
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
